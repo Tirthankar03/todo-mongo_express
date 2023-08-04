ff347ab466b5b0f16f876995542461396d8e90c0
Tirthankar Nath <tirthankar.nath2@gmail.com>
2023-08-04 05:54:00 +0530

    app now able to create task

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const Task = require('./models/task');
 const app = express();
 
 //creates a middleware: express becomes able to check if the requested static files are from the ./views and sends the response (ejs, html,css)
-const staticFiles = express.static("./views");
-app.use(staticFiles);
+// const staticFiles = express.static("./views");
+// app.use(staticFiles);
 
 //to use encrypted files
 app.use(express.urlencoded());
@@ -21,45 +21,62 @@ app.use(express.urlencoded());
 app.use('view engine', 'ejs');
 app.set('views', './views');
 
-//creating a custom error handler middleware
-const errorHandler = (err, req, res, next) => {
-    console.error("Error in fetching tasks from db: ", err);
-    res.status(500).send("something went wrong!");
-};
-//rendering the App page
-app.get('/', async(req, res) => {
-    try {
-        const tasks = await Task.find({});
-        res.render('home', {title: 'Home', tasks: tasks});
-    } catch (err) {
-        next(err);
+// rendering the App Page
+// app.get('/', function(req, res){
+//     Task.find({}, function(err, task){
+//         if(err){
+//             console.log('Error in fetching tasks from db');
+//             return;
+//         }
+
+//         return res.render('home', {
+//             tittle: "Home",
+//             task: task
+//         });
+//     }
+// )});
+
+app.get('/', (req, res) =>{
+    Task.find({}, (err, task) =>{
+        if(err){
+            console.log('Error in fetching tasks from db');
+        }else{    
+        res.render('home', {
+            title: "Home",
+            tasks: task
+        });
+        }
     }
+)});
+
+//creating tasks
+app.post('/create', (req, res) =>{
+    Task.create({},(err, newTask) =>{
+        if (err) {
+            console.log('error creating task', err);
+        } else {
+            //redirects back to / i.e home page where it again encounters get request to see all the tasks in the db
+            res.redirect('back');
+        }
+    });
 });
 
-//attach the error handling middleware at the end of your middleware
-app.use(errorHandler);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//create tasks
 
 //delete tasks
 
+
+
+
+
+
+
+
+
 //make the app listen on assigned port
+app.listen(port, (err)=>{
+    if (err) {
+        console.log(`error in running the server: ${err}`);
+    } else {
+        console.log(`server is running on port: ${port}`);
+    }
+});
